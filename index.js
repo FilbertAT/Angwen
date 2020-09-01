4c237cfc2359a521a583357262759e78df6592f3
FilbertAT <58982328+FilbertAT@users.noreply.github.com>
2020-09-01 23:47:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber = Math.floor(Math.random() * 6);
 var randomNumber2 = Math.floor(Math.random() * 6);
 
 var picsMingYuArray = ["pics/MY1.jpg", "pics/MY2.jpg", "pics/MY3.png", "pics/MY4.jpg", "pics/MY5.jpg", "pics/MY6.jpg"];
-var picsAngwenArray = ["pics/Angwen1.jpg", "pics/Angwen2.jpg", "pics/Angwen3.jpg", "pics/Angwen4.jpg", "pics/Angwen5.jpg", "pics/Angwen6.jpg"]
+var picsAngwenArray = ["pics/Angwen1.jpg", "pics/Angwen2.jpg", "pics/Angwen3.jpg", "pics/Angwen4.jpg", "pics/Angwen5.jpg", "pics/Angwen6.jpg"];
 var picsChosen = picsMingYuArray[randomNumber];
 var picsChosen2 = picsAngwenArray[randomNumber2];
 
@@ -16,6 +16,6 @@ if (randomNumber === 3) {
 
 } else {
 
-  document.querySelector("h1").innerHTML = "Sweet! You're Kim MingYu Future Waifu <3"
+  document.querySelector("h1").innerHTML = "Sweet! You're Kim MingYu Future Waifu <3";
 
 }
